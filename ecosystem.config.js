f73a045907751ea53a9c8ba85e88049cf294b018
Ubuntu <ubuntu@ip-172-31-28-88.eu-north-1.compute.internal>
2025-08-07 14:17:09 +0000

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     {
       name: "frontend",
       script: "serve",
-      args: "-s /var/www/deploy-frontend -l 3000  --listen 0.0.0.0",
+      //args: "-s /var/www/deploy-frontend -l 3000  --listen 0.0.0.0",
+      args: "-s /var/www/deploy-frontend --listen http://0.0.0.0:3000",
       interpreter: "none"
     }
   ]
